e75fe557c73afaa0c74aa5584a19c12563d99891
sagar jagade <sagarjagade2023@gmail.com>
2026-05-20 11:58:30 +0530

    Header,home,footer page binding

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-createRoot(document.getElementById('root')).render(
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
